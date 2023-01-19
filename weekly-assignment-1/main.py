3c76079ff49a8e7612f34e670e4a4d399d28cdda
IsacWenn <s01isin79@gmail.com>
2023-01-19 14:08:22 +0100

    Update main.py

--- weekly-assignment-1/main.py
+++ weekly-assignment-1/main.py
@@ -6,24 +6,36 @@ import mplcursors
 YEAR = 2020
 
 def filter_csv_data_by_year(pandas_csv_data: pandas.DataFrame, year: int):
-
     return pandas_csv_data[pandas_csv_data["Year"] == year].drop(columns=["Year", "Code"])
 
-def construct_scatter_plot(gdp_data, life_expectancy_data):
+def construct_scatter_plot(gdp_data: pandas.DataFrame, life_expectancy_data: pandas.DataFrame):
     data = gdp_data.merge(life_expectancy_data)
     for row in data.iterrows():
-        row = row[1]
+        row = row[1] # Converts row from a Tuple[Hashable, Series] to a Series
         plt.scatter(row["GDPPC"], row["Life expectancy"], marker=".")
 
 def main():
+
+    #
+    # Reading the data.
+    #
     gdp_data = pandas.read_csv("data/gdp-per-capita-worldbank.csv")
     life_expectancy_data = pandas.read_csv("data/life-expectancy.csv")
 
+    #
+    # Filtering the data to only include data points from a specific year.
+    #
     gdp_data_year = filter_csv_data_by_year(gdp_data, YEAR)
     life_expectancy_data_year = filter_csv_data_by_year(life_expectancy_data, YEAR)
 
+    #
+    # Constructing a scatter plot with the filtered data.
+    #
     construct_scatter_plot(gdp_data_year, life_expectancy_data_year)
 
+    #
+    # Finishing touches and details for the plot.
+    #
     plt.xlabel("GDP per capita")
     plt.ylabel("Life expectancy at birth")
     plt.show()
