aaaa62c55f93dead9d4a668b54cbe0ae926dfa95
IsacWenn <s01isin79@gmail.com>
2023-01-19 14:03:06 +0100

    finished

--- weekly-assignment-1/main.py
+++ weekly-assignment-1/main.py
@@ -1,13 +1,19 @@
 import pandas
 import numpy
 import matplotlib.pyplot as plt
+import mplcursors
 
 YEAR = 2020
 
 def filter_csv_data_by_year(pandas_csv_data: pandas.DataFrame, year: int):
 
-    return pandas_csv_data[pandas_csv_data["Year"] == year]
+    return pandas_csv_data[pandas_csv_data["Year"] == year].drop(columns=["Year", "Code"])
 
+def construct_scatter_plot(gdp_data, life_expectancy_data):
+    data = gdp_data.merge(life_expectancy_data)
+    for row in data.iterrows():
+        row = row[1]
+        plt.scatter(row["GDPPC"], row["Life expectancy"], marker=".")
 
 def main():
     gdp_data = pandas.read_csv("data/gdp-per-capita-worldbank.csv")
@@ -16,9 +22,10 @@ def main():
     gdp_data_year = filter_csv_data_by_year(gdp_data, YEAR)
     life_expectancy_data_year = filter_csv_data_by_year(life_expectancy_data, YEAR)
 
-    
+    construct_scatter_plot(gdp_data_year, life_expectancy_data_year)
 
-    plt.scatter(10, 10)
+    plt.xlabel("GDP per capita")
+    plt.ylabel("Life expectancy at birth")
     plt.show()
 
 
