47983185b89f1591e7955049fc9d4120e9b6c5ff
IsacWenn <s01isin79@gmail.com>
2023-01-19 13:29:12 +0100

    Update main.py

--- weekly-assignment-1/main.py
+++ weekly-assignment-1/main.py
@@ -2,6 +2,7 @@ import pandas
 import numpy
 import matplotlib.pyplot as plt
 
+YEAR = 2020
 
 def filter_csv_data_by_year(pandas_csv_data: pandas.DataFrame, year: int):
 
@@ -12,8 +13,10 @@ def main():
     gdp_data = pandas.read_csv("data/gdp-per-capita-worldbank.csv")
     life_expectancy_data = pandas.read_csv("data/life-expectancy.csv")
 
-    gdp_data_2020 = filter_csv_data_by_year(gdp_data, 2020)
-    print(gdp_data_2020)
+    gdp_data_year = filter_csv_data_by_year(gdp_data, YEAR)
+    life_expectancy_data_year = filter_csv_data_by_year(life_expectancy_data, YEAR)
+
+    
 
     plt.scatter(10, 10)
     plt.show()
