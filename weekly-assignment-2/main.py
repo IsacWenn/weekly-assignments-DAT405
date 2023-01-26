4d4cb9eceb44cdbbbab63c58b3a848fdeb7abe3e
Pontanami <pontusen@student.chalmers.se>
2023-01-26 12:22:35 +0100

    Setup

---
+++ weekly-assignment-2/main.py
@@ -0,0 +1,14 @@
+import pandas
+import numpy
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+
+
+def main():
+    print("hej")
+
+
+if __name__ == "__main__":
+    main()
