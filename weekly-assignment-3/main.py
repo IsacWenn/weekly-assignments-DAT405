42281cea5ed8d4f176dd78d58cc2e7a381e3dbe5
Pontanami <pontusen@student.chalmers.se>
2023-02-06 13:15:27 +0100

    1 done

--- weekly-assignment-3/main.py
+++ weekly-assignment-3/main.py
@@ -10,13 +10,20 @@ matplotlib.use("TkAgg")
 def main():
     data = pandas.read_csv("data/data_assignment3.csv")
     figure, axis = plt.subplots(1, 2)
-    axis[0].scatter(data["phi"], data["psi"], marker=".")
+    axis[0].scatter(data["phi"], data["psi"], marker=".", s=1)
     axis[0].set_xlabel("phi")
     axis[0].set_ylabel("psi")
-    axis[1].hist2d(data["phi"], data["psi"], bins=20)
+    axis[1].hist2d(data["phi"], data["psi"], bins=20, cmap="Blues")
+    axis[1].set_xlabel("phi")
+    axis[1].set_ylabel("psi")
 
-    plt.xlabel("phi")
-    plt.ylabel("psi")
+    PCM = None
+    ax = plt.gca()  # get the current axes
+    for PCM in ax.get_children():
+        if isinstance(PCM, plt.cm.ScalarMappable):
+            break
+
+    plt.colorbar(PCM, ax=ax)
     plt.show()
 
 
