49f8cd9cd253645fe2f8fcece4957803b9d891c8
Pontanami <pontusen@student.chalmers.se>
2023-02-06 12:35:31 +0100

    main and data dir push

--- weekly-assignment-3/main.py
+++ weekly-assignment-3/main.py
@@ -1,1 +1,15 @@
 # Main Python program (Week 3)
+import numpy as np
+import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use("TkAgg")
+
+
+def main():
+    data = pandas.read_csv("data/data_assignment3.csv")
+
+
+if __name__ == "__main__":
+    main()
