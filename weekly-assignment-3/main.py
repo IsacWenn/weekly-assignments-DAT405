daade8252ef412cba7b030a29681bc01e1a300ca
Pontanami <pontusen@student.chalmers.se>
2023-02-06 12:55:02 +0100

    scatter plot and 2d histogram

--- weekly-assignment-3/main.py
+++ weekly-assignment-3/main.py
@@ -9,6 +9,15 @@ matplotlib.use("TkAgg")
 
 def main():
     data = pandas.read_csv("data/data_assignment3.csv")
+    figure, axis = plt.subplots(1, 2)
+    axis[0].scatter(data["phi"], data["psi"], marker=".")
+    axis[0].set_xlabel("phi")
+    axis[0].set_ylabel("psi")
+    axis[1].hist2d(data["phi"], data["psi"], bins=20)
+
+    plt.xlabel("phi")
+    plt.ylabel("psi")
+    plt.show()
 
 
 if __name__ == "__main__":
