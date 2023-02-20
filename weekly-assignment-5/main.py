dc53ae383fee80f5e8ab0e9f80d5f44357ee1d56
vistca <93658028+vistca@users.noreply.github.com>
2023-02-20 13:54:45 +0100

    Pontus needs commit

--- weekly-assignment-5/main.py
+++ weekly-assignment-5/main.py
@@ -9,14 +9,28 @@ def main():
 
 # V_k[s] = max_a Σ_s' p(s′|s,a)*(r(a,s,s′) + γ*V_k−1[s′])
 
+
+def actions(x_coord: float, y_coord: float, x_dim: float, y_dim: float):
+    action_list = []
+    if x_coord > x_dim or x_coord < 0 or y_coord > y_dim or y_coord < 0:
+        return action_list
+    if x_coord + 1 <= x_dim:
+        action_list.append((x_coord + 1, y_coord))
+    if y_coord + 1 <= y_dim:
+        action_list.append((x_coord, y_coord + 1))
+    if x_coord - 1 >= 0:
+        action_list.append((x_coord - 1, y_coord))
+    if y_coord - 1 >= 0:
+        action_list.append((x_coord, y_coord - 1))
+    return action_list
+
+
 def value_iteration(array: list, reward: list, gamma: float, epsilon: float):
     V_k = numpy.zeros(len(array))
     diff = 1
     while diff > epsilon:
         for state in array:
             pass
-
-
 
 
 if __name__ == "__main__":
