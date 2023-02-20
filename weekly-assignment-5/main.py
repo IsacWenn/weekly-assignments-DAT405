a3fdd22e0a4d2b4f248f79be97764244b8243c39
Pontanami <pontusen@student.chalmers.se>
2023-02-20 13:36:04 +0100

    carl need push very mucho

--- weekly-assignment-5/main.py
+++ weekly-assignment-5/main.py
@@ -1,5 +1,22 @@
+import numpy
+
+
 def main():
-    print("Hello")
+    reward = numpy.array([[0, 0, 0, 0, 10, 0, 0, 0, 0]])
+    epsilon = 0.01
+    gamma = 0.9
+
+
+# V_k[s] = max_a Σ_s' p(s′|s,a)*(r(a,s,s′) + γ*V_k−1[s′])
+
+def value_iteration(array: list, reward: list, gamma: float, epsilon: float):
+    V_k = numpy.zeros(len(array))
+    diff = 1
+    while diff > epsilon:
+        for state in array:
+            pass
+
+
 
 
 if __name__ == "__main__":
